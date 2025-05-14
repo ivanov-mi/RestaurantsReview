913fa28e37273cc75e1a5b6b4419a6f12f7c19d5
Martin Ivanov <ivanov-mi@outlook.com>
2025-05-14 16:18:04 +0300

    Refactor RestaurantListTableViewController

--- RestaurantsReview/Screens/RestaurantList/ViewControllers/RestaurantListTableViewController.swift
+++ RestaurantsReview/Screens/RestaurantList/ViewControllers/RestaurantListTableViewController.swift
@@ -11,6 +11,7 @@ class RestaurantListViewController: UITableViewController {
     
     // TODO: Update after adding local persistance
     
+    // MARK: - Properties
     lazy var restaurants: [Restaurant] = {
         TestDataProvider.shared.sampleRestaurants
     }()
@@ -18,38 +19,49 @@ class RestaurantListViewController: UITableViewController {
     // MARK: VC Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Restaurants"
-
-        tableView.register(RestaurantListTableViewCell.nib, forCellReuseIdentifier: RestaurantListTableViewCell.identifier)
-        tableView.separatorStyle = .none
+        configureView()
     }
     
-    // MARK: Override
+    private func configureView() {
+        title = "Restaurants"
+        tableView.separatorStyle = .none
+        tableView.register(RestaurantListTableViewCell.nib, forCellReuseIdentifier: RestaurantListTableViewCell.identifier)
+    }
+    
+    // MARK: - Table View Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return restaurants.count
     }
 
+    // MARK: - Table View Delegate
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: RestaurantListTableViewCell.identifier, for: indexPath) as! RestaurantListTableViewCell
         let restaurant = restaurants[indexPath.row]
         
         cell.nameLabel.text = restaurant.name
-        cell.ratingLabel.text = restaurant.rating.map { String(format: "%.1f", $0) } ?? "N/A"
+        cell.ratingLabel.text = restaurant.rating.map { String(format: "%.1f", $0) } ?? "Not rated"
         cell.restaurantImageView.image = restaurant.imagePath.flatMap { UIImage(named: $0) } ?? UIImage(systemName: "photo")
 
         return cell
     }
 
+    // MARK: - Table View Delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        navigateToRestaurantDetails(for: restaurants[indexPath.row])
+    }
+    
+    // MARK: - Navigation
+    func navigateToRestaurantDetails(for restaurant: Restaurant) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         if let detailsVC = storyboard.instantiateViewController(withIdentifier: "RestaurantDetailsViewController") as? RestaurantDetailsViewController {
-            detailsVC.restaurant = restaurants[indexPath.row]
+            detailsVC.restaurant = restaurant
             detailsVC.delegate = self
             navigationController?.pushViewController(detailsVC, animated: true)
         }
     }
 }
 
+// MARK: - RestaurantDetailsViewControllerDelegate
 extension RestaurantListViewController: RestaurantDetailsViewControllerDelegate {
     func didUpdateRestaurant(_ restaurant: Restaurant) {
         if let index = restaurants.firstIndex(where: { $0.name == restaurant.name }) {
